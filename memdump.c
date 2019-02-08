061683997ca1191dafa19cf178d145bfefac0c85
Hidemi <hidemi@aquaxis.com>
2019-02-08 09:05:46 +0900

    Modify

--- memdump.c
+++ memdump.c
@@ -2,43 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
-#define MAP_LENG 0x00100000
+#define PAGE_NUM 1024
 
-int main(int argc, char **argv){
+void main(int argc, char **argv){
   int fd, i;
   unsigned int *addr;
   unsigned int offset, count;
-  unsigned int ret;
+  long page_size, map_size;
+
+  if(argc < 2){
+    printf("Usage: %s ADDRESS(hex) NUM(dec)\n", argv[0]);
+    return;
+  }
 
   fd = open( "/dev/mem", O_RDWR | O_SYNC );
   if( fd == -1 ){
     printf( "Can't open /dev/mem.\n" );
-    return 0;
+    return;
   }
 
-  offset = strtol( argv[1], NULL, 16 );
-  count = strtol( argv[2], NULL, 10 );
+  offset = strtoul( argv[1], NULL, 16 );
+  count  = strtoul( argv[2], NULL, 10 );
 
-  addr = mmap( NULL, MAP_LENG, 
-               PROT_READ | PROT_WRITE,
-			   MAP_SHARED, fd, 
-			   offset & 0xFFFF0000);
+  page_size = getpagesize();
+  map_size  = page_size * PAGE_NUM;
+
+  // Assign memory map
+  addr = mmap( NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset & 0xFFFF0000);
   if( addr == MAP_FAILED ){
     printf( "Error: mmap()\n" );
-    return 0;
+    return;
   }
 
-  printf( "usage: memdump ADRS(hex) LEN(dec)\n" );
-
+  // Read
   for( i = 0; i < count; i++ ){
     printf( "%08X: %08X\n",
             ( (offset & 0xFFFFFFFC) + (i * 4) ),
             ( addr[((offset & 0x0000FFFC) /4 ) + i] ) );
-    ret = ( addr[((offset & 0x0000FFFC) /4 ) + i] );
   }
-  printf( "\n" );
 
-  return ret;
+  // Free
+  munmap(addr, map_size);
+  close(fd);
+
+  return;
 }
 
--- memwrite.c
+++ memwrite.c
@@ -2,42 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
-#define MAP_LENG 0x00100000
+#define PAGE_NUM 1024
 
 void main(int argc, char **argv){
-  int fd, i;
+  int fd;
   unsigned int *addr;
   unsigned int offset, data;
+  long page_size, map_size;
+
+  if(argc < 2){
+    printf("Usage: %s ADDRESS(hex) DATA(hex)\n", argv[0]);
+    return;
+  }
 
   fd = open( "/dev/mem", O_RDWR );
   if( fd == -1 ){
     printf( "Can't open /dev/mem.\n" );
-    return ;
+    return;
   }
 
-  offset = strtol( argv[1], NULL, 16 );
-  data   = strtol( argv[2], NULL, 16 );
+  page_size = getpagesize();
+  map_size  = page_size * PAGE_NUM;
 
-  addr = mmap( NULL, MAP_LENG, 
-               PROT_READ | PROT_WRITE, 
-			   MAP_SHARED, fd, 
-			   offset & 0xFFFF0000);
+  offset = strtoul( argv[1], NULL, 16 );
+  data   = strtoul( argv[2], NULL, 16 );
+
+  // Assign memory map
+  addr = mmap( NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset & 0xFFFF0000);
   if( addr == MAP_FAILED ){
     printf( "Error: mmap()\n" );
-    return ;
+    return;
   }
 
-  printf( "usage: memdump ADRS(hex) LEN(dec)\n" );
-
+  // Write
   addr[(offset & 0x0000FFFC) / 4] = data;
 
-  printf( "%08X: %08X\n", 
-          ( offset & 0xFFFFFFFC ),
-          ( addr[(offset & 0x0000FFFC) /4] ) );
+  // Read
+//  printf( "%08X: %08X\n", ( offset & 0xFFFFFFFC ), ( addr[(offset & 0x0000FFFC) /4] ) );
 
-  printf( "\n" );
+  // Free
+  munmap(addr, map_size);
+  close(fd);
 
-  return ;
+  return;
 }
 
